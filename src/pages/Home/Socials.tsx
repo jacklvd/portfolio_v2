bb5e68df3ae350780828c15806ab0129a00c1a6f
jacklvd <vodanglong3103@gmail.com>
2023-09-16 00:33:17 -0400

    update last stuff

--- src/pages/Home/Socials.tsx
+++ src/pages/Home/Socials.tsx
@@ -18,7 +18,7 @@ function Social() {
                 <a href="https://www.linkedin.com/in/itsmejack/" className="home_social-icon" target="_blank" rel="noopener noreferrer">
                     <i className="uil uil-linkedin-alt"></i>
                 </a>
-                <a href="https://drive.google.com/file/d/1RuUkVwUtv5crPAh-cS6EjJ1UAL4xw2Yd/view?usp=sharing" className="home_social-icon" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1CCvBSb8x1mBjsLrzZfoGrkdRRLByS_8t/view?usp=sharing" className="home_social-icon" target="_blank" rel="noopener noreferrer">
                     <i className="uil uil-bag-alt"></i>
                 </a>
 
