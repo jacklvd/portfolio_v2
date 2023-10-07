f5f7c274aeedb75a8c2f19d290dad656ecec6986
jacklvd <vodanglong3103@gmail.com>
2023-10-07 00:30:41 -0400

    add footer and fix responsive 280px

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import './styles/footer.scss'
+
+const Footer = () => {
+    return (
+        <section className="footer_section">
+            <div className="footer">
+                <p className="enclosed">Copyright &copy; Jack Vo. All rights reserved.</p>
+                <p className="year">2023</p>
+            </div>
+
+        </section>
+    )
+}
+
+export default Footer
--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -5,6 +5,7 @@ import Journey from '../../components/journey/Journey'
 import Work from '../../components/work/Work'
 import Polaroids from '../../components/polaroid/Polaroids'
 import Contact from '../../components/contact/Contact'
+import Footer from '../../components/Footer'
 
 const Portfolio = () => {
     return (
@@ -18,6 +19,7 @@ const Portfolio = () => {
                 <Work />
                 <Polaroids />
                 <Contact />
+                <Footer />
             </main>
         </>
     )
